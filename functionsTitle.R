441b2bd869fa5548958a1b387b80b89fc19bc913
aamodini <gupta.aamodini@gmail.com>
2019-04-13 01:23:37 -0500

    dplyr and data.tables to speed up shiny

--- functionsTitle.R
+++ functionsTitle.R
@@ -1,86 +1,47 @@
-# Libraries and Data ---------------------------------------------------------------
-library(sqldf)
+# Load libraries and relevant data ---------------------------------------------------------------
+library(dplyr)
 library(readr)
 library(data.table)
 
-# principal<-as.data.frame(fread("title.principals.tsv"))
-# crew <- as.data.frame(fread("rawData/title.crew.tsv"))
-
-# akas <- as.data.frame(fread("title.akas.tsv"))
-# title.basic <- as.data.frame(fread("title.basics.tsv"))
-# name.basic <- as.data.frame(fread("name.basics.tsv"))
-# staff_movie <- as.data.frame(fread("staff_movie.tsv"))
+title.basic <- as.data.table(readRDS("../IMDbRDS/title.basic.rds"))
+name.basic <- as.data.table(readRDS("../IMDbRDS/name.basic.rds"))
+staff_movie <- as.data.table(readRDS("../IMDbRDS/staff_movie2.rds"))
 
 # Name to NameID ----------------------------------------------------------
 
 get.nameID <- function (name.input) {
-  name.matrix <- matrix(NA, 1, length(name.input)) # matrix for storage
-  
-  # Name in characters to nameID 
-  
-  for (i in 1:length(name.input)){
-    name.matrix[1,i] <- name.basic[which(name.basic$primaryName == name.input[i]), 1]
-  }
-  
-  return(name.matrix) # outputs 2 corresponding name IDs 
+  name.list <- name.basic %>% select(nconst, primaryName) %>% 
+    filter(primaryName %in% name.input) %>% select(nconst)
+  return(name.list) # outputs all possible nconsts
 }
+
 
 # NameID to Name ----------------------------------------------------------
 
-# had to create this function to test.
+# Test function
 
 get.name <- function (n) {
-  nameID.matrix <- matrix(NA, 1, length(n)) # matrix for storage
-  
-  # Name in characters to nameID 
-  
-  for (i in 1:length(n)){
-    nameID.matrix[1,i] <- name.basic[which(name.basic$nconst == n[i]), 2]
-  }
-  
-  return(nameID.matrix) # outputs 2 corresponding name IDs 
+  nameID.list <- name.basic %>% select(nconst, primaryName) %>% 
+    filter(nconst %in% n) %>% select(primaryName)
+  return(nameID.list) # outputs a name for every nconsts 
 }
 
 # Function: Use Name and Title ID -----------------------------------------
 
-name.title.id <- function (n1, n2) {
-  # nameID <- c(n1, n2) # this will be the input converted to the name ID
-  # name.index <- sapply(1:length(nameID), function (x) which(colnames(staff_movie) == nameID[x])) #gives name indices 
-  # df_interest <- data.frame(staff_movie[,name.index])
-  # 
-  # # get nameID to recognize in SQL syntax
-  # getQuote <- names(sqldf("SELECT * FROM df_interest WHERE 1 = 0"))
-  # 
-  # # generalize the query
-  # query <- paste0("SELECT ", getQuote[1]," FROM df_interest INTERSECT SELECT ", getQuote[2], " FROM df_interest")
-  # intersect.sql <- sqldf(query)
-  # return(intersect.sql)
-  
-  x <- staff_movie[,colnames(staff_movie) == n1]
-  y <- staff_movie[,colnames(staff_movie) == n2]
-  return(data.frame(int=intersect(x,y)))
+name.title.id <- function (n) {
+  df <- staff_movie[,n, with = FALSE]
+  int = intersect(na.omit(df[,n[1],with=F][[1]]),na.omit(df[,n[2],with=F][[1]]))
+  return(int)
 }
 
-# n <-  c("nm0760044", "nm0665381")
-
+n <-  c("nm0760044", "nm0665381")
+titleID <- name.title.id(n)
 
 # TitleID to Title Name --------------------------------------------------------
 
 get.title <- function(titleID){
-  
-  if (sum(is.na(titleID)) == 0){
-    titleID <- titleID
-  } else {
-    titleID <- titleID[complete.cases(titleID),]
-  }
-  
-  titles <- matrix(NA, 1, 1000)
-  
-  for (i in 1:length(titleID)){
-    titles[1,i] <- title.basic[which(title.basic$tconst == titleID[i]), 3]
-    title.name <- titles[!is.na(titles)]
-  }
-  
+  title.name <- title.basic %>% select(tconst, primaryTitle) %>% 
+    filter(tconst %in% titleID) %>% select(primaryTitle)
   return(title.name)
 }
 
