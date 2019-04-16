b2a412fe22e93ea9b5589881950bbe124cf06a82
aamodini <gupta.aamodini@gmail.com>
2019-04-16 00:31:48 -0500

    add visualizations and organize the shiny app

--- functionsTitle.R
+++ functionsTitle.R
@@ -3,9 +3,14 @@ library(dplyr)
 library(readr)
 library(data.table)
 
+library(tidyverse)
+library(igraph)
+library(RColorBrewer)
+
 title.basic <- as.data.table(readRDS("../IMDbRDS/title.basic.rds"))
 name.basic <- as.data.table(readRDS("../IMDbRDS/name.basic.rds"))
 staff_movie <- as.data.table(readRDS("../IMDbRDS/staff_movie2.rds"))
+rating <- as.data.table(readRDS("../IMDbRDS/title.ratings.rds")) 
 
 # Name to NameID ----------------------------------------------------------
 
@@ -96,6 +101,140 @@ name.title.characters <- function(names, titlesKnown) {
   # get title name (use primaryName)
   return(get.title(titleID))
   
+}
+
+
+# Visualizations ----------------------------------------------------------
+
+### --------------- Network between two staff ------------------- 
+
+
+#######################
+## Fig. of Time Line ##
+#######################
+
+get.sim.movie <- function(names,titlesKnown){
+  if (dim(get.nameID(names))[1] == 2){
+    nameID <- get.nameID(names)
+  } else {
+    nameID <- specNameID(names, titlesKnown)
+  }
+  
+  id1 <- nameID[[1]][1]
+  id2 <- nameID[[1]][2]
+  
+  ### find similar movies
+  link <- name.title.id(c(id1,id2))
+  
+  sim.movie <- as.data.table(link)
+  sim.movie$from <- id1
+  sim.movie$to <- id2
+  sim.movie$link <- as.character(sim.movie$link)
+  
+  ### find individual movies
+  movie1 <- na.omit(staff_movie[,id1, with = FALSE])
+  names(movie1)[1] <- "link"
+  movie1$link <- as.character(movie1$link)
+  
+  movie2 <- na.omit(staff_movie[,id2, with = FALSE])
+  names(movie2)[1] <- "link"
+  movie2$link <- as.character(movie2$link)
+  
+  ### add info of movies' releasing year and ratings
+  basics <- title.basic
+  names(basics)[1] <- "link"
+  names(rating)[1] <- "link"
+  
+  # match all by movie ids
+  junk <- left_join(sim.movie, basics, by = "link")
+  junk <- left_join(junk, rating, by = "link")
+  sim.movie <- select(junk, c(from, to, link, startYear, primaryTitle, averageRating))
+  
+  junk <- left_join(movie1, rating, by = "link")
+  movie1 <- select(junk, c(link, averageRating))
+  
+  junk <- left_join(movie2, rating, by = "link")
+  movie2 <- select(junk, c(link, averageRating))
+  
+  # create a factor identifying decades 
+  sim.movie$decades <- cut(as.numeric(sim.movie$startYear), 
+                           breaks = seq(1920, 2020, 10),
+                           labels = paste(seq(1920, 2010, 10),"s",sep = ""))
+  return(sim.movie)
+}
+
+timeLine.df <- function(names, titlesKnown){
+  
+  sim.movie <- get.sim.movie(names, titlesKnown)
+  # count number of movies in each decades
+  plot.df <- as.data.frame(table(sim.movie$decades))
+  
+  # first time co-op
+  first <- head(which(plot.df$Freq > 0), n = 1)
+  
+  # last time co-op
+  last <- tail(which(plot.df$Freq > 0), n = 1)
+  
+  # filter the data to
+  plot.df <- plot.df[first:last,]
+  
+  return(plot.df)
+}
+
+
+
+#######################
+### Fig. of Ratings ###
+#######################
+
+# function for creating the rate categorys
+rate_cat <- function(movie.df) {
+  # input is a data frame containing a column named "averageRating"
+  # output is the same data frame with extra column named "rate"
+  movie.df$rate <- cut(as.numeric(movie.df$averageRating),
+                       breaks = seq(0, 10, 2),
+                       labels = c("Terrible (IMDB 0~2)",
+                                  "Bad (IMDB 2~4)",
+                                  "Mediocre (IMDB 4~6)",
+                                  "Good (IMDB 6~8)",
+                                  "Excellent (IMDB 8~10)"))
+  return(movie.df)
+}
+
+pieCharts.df <- function(names, titlesKnown){
+  
+  sim.movie <- get.sim.movie(names, titlesKnown)
+  sim.movie <- rate_cat(sim.movie)
+  movie1 <- rate_cat(movie1)
+  movie2 <- rate_cat(movie2)
+  
+  # create plot df
+  plot.df <- as.data.frame(table(sim.movie$rate))
+  plot.df$type <- "Together [unit: %]"
+  
+  temp.df <- as.data.frame(table(movie1$rate))
+  temp.df$type <- paste(names[1],"[unit:%]")
+  plot.df <- rbind(plot.df, temp.df)
+  
+  temp.df <- as.data.frame(table(movie2$rate))
+  temp.df$type <- paste(names[2],"[unit:%]")
+  plot.df <- rbind(plot.df, temp.df)
+  
+  plot.df <- plot.df %>% group_by(type) %>%
+    mutate(pct = round((Freq / sum(Freq)*100),0))
+  plot.df$pct[plot.df$Freq == 0] <- NA
+  
+  # plot a pie chart
+  blank_theme <- theme_minimal()+
+    theme(
+      axis.title.x = element_blank(),
+      axis.title.y = element_blank(),
+      panel.grid=element_blank(),
+      axis.ticks = element_blank(),
+      plot.title=element_text(size=14, face="bold")
+    )
+  
+  return(plot.df)
 }
 
 
--- title_app.R
+++ title_app.R
@@ -1,6 +1,6 @@
 library(shiny)
 library(DT)
-# source("intersectionFunc.R")
+source("functionsTitle.R")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -8,28 +8,54 @@ ui <- fluidPage(
    # Application title
    titlePanel("Find Similar Movies"),
    
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-        textInput("nm1", "Enter Name 1", value = "Hrishikesh Mukherjee"),
-        textInput("tt1", "Enter Movie 1", value = "Anand"),
-        textInput("nm2", "Enter Name 2", value = "Sachin Bhowmick"),
-        textInput("tt2", "Enter Movie 2", value = "Jhooth Bole Kauwa Kaate")
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-        verbatimTextOutput("value")
-        #tabPanel("mtcars", DT::dataTableOutput("dataTable"))
-      )
-   )
+   hr(),
+   
+   fluidRow(
+     column(3,
+            h4("Pick Names")
+     ),
+     column(4, offset = 1,
+            textInput("nm1", "Enter Name 1", value = "Hrishikesh Mukherjee"),
+            textInput("tt1", "Enter Movie 1", value = "Anand")
+     ),
+     column(4,
+            textInput("nm2", "Enter Name 2", value = "Sachin Bhowmick"),
+            textInput("tt2", "Enter Movie 2", value = "Jhooth Bole Kauwa Kaate")
+     )
+   ),
+   
+   dataTableOutput("movieList"),
+   
+   plotOutput("timeLine"),
+   
+   plotOutput("pieCharts")
+   
+   
+   
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  output$value <- renderPrint({
-    name.title.characters(c(input$nm1, input$nm2), c(input$tt1, input$tt2))
-    # staff_movie[, colnames(staff_movie) == c(input$nm1, input$nm2)]
+  output$timeLine <- renderPlot({
+    ggplot(timeLine.df(c(input$nm1, input$nm2), c(input$tt1, input$tt2)), aes(x=Var1, y=Freq, group=1)) +
+      geom_point(stat='summary', fun.y=sum) + 
+      stat_summary(fun.y=sum, geom="line") + 
+      expand_limits(y = 0) + 
+      labs(x = "Decade", y = "Number of co-op movies", title = "Movies Over Time")
+  })
+  output$movieList <- renderDataTable({
+    n <- c("primaryTitle","startYear")
+    get.sim.movie(c(input$nm1, input$nm2), c(input$tt1, input$tt2))[,n]
+  })
+  output$pieCharts <- renderPlot({
+    ggplot(pieCharts.df(c(input$nm1, input$nm2), c(input$tt1, input$tt2)), aes(x="", y=Freq, fill=Var1))+
+      geom_bar(stat = "identity", position = position_fill()) +
+      geom_text(aes(label = pct), position = position_fill(vjust = 0.5),size = 3) +
+      scale_fill_brewer(palette = "RdYlGn", drop = F) +
+      labs(fill = "Rate Category") +
+      coord_polar("y", start=0) +
+      facet_grid(.~type) + blank_theme +
+      theme(axis.text.x=element_blank())
   })
 }
 
